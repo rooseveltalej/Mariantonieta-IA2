acb3f4045609be6bff454b1dbf85161d5d8e384a
Roosevelt Alejandro <roperez@estudiantec.cr>
2025-10-13 15:54:50 -0600

    Add constants for file paths and directories
    - Created a new constants.py file to define base directory, data directory, and CSV file path for Bitcoin prices.
    - Added logic to resolve the base directory dynamically based on the file's location.

---
+++ notebooks/constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+# Directorio base (la carpeta donde está este archivo)
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Directorio donde guardar/leer datos
+DATA_DIR = BASE_DIR / "data/raw"
+# Nombre del archivo CSV con precios de bitcoin
+BITCOIN_CSV_FILENAME = "bitcoin_price_Training - Training.csv"
+# Ruta completa al CSV (Path object)
+CSV_BITCOIN_PATH = DATA_DIR / BITCOIN_CSV_FILENAME
+
+
+
